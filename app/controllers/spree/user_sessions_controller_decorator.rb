d1db33d4bee5f47ccabe9c0888012e83c23b9478
Thomas Boltze <thomas.boltze@gmail.com>
2012-08-27 14:29:29 +0800

    Redirect user to home page after login Made fedex test label appear on all fedex shipping methods

---
+++ app/controllers/spree/user_sessions_controller_decorator.rb
@@ -0,0 +1,22 @@
+# Purpose: after the user logs in, he shold be redirected to the home page instead of the product page, unless there is a redirect location stored.
+Spree::UserSessionsController.class_eval do
+  def create
+    authenticate_user!
+
+    if user_signed_in?
+      respond_to do |format|
+        format.html {
+          flash.notice = t(:logged_in_succesfully)
+          redirect_back_or_default(root_path)
+        }
+        format.js {
+          user = resource.record
+          render :json => {:ship_address => user.ship_address, :bill_address => user.bill_address}.to_json
+        }
+      end
+    else
+      flash.now[:error] = t('devise.failure.invalid')
+      render :new
+    end
+  end
+end
