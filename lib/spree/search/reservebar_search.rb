99f5301cf12d3127e38e370bf870fd2973a73c57
Jindong Yang <yangjindong@gmail.com>
2012-09-19 21:39:19 +0800

    Search that matches category or family name

--- lib/spree/search/reservebar_search.rb
+++ lib/spree/search/reservebar_search.rb
@@ -1,9 +1,17 @@
 module Spree::Search
 	class ReservebarSearch < Spree::Core::Search::Base
 
+    def retrieve_products
+      base_scope = get_base_scope
+      @products_scope = @product_group.apply_on(base_scope)
+      curr_page = manage_pagination && keywords ? 1 : page
+
+      @products = @products_scope.includes([:images, :master, :taxons]).page(curr_page).per(per_page)
+    end
+
 		# method should return new scope based on base_scope
 		def get_products_conditions_for(base_scope, query)
-			base_scope.rlike_any([:name], query.split)
+			base_scope.rlike_any_or_in_taxons([:name], query.split)
 		end
 
 	end
