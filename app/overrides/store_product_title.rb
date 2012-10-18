900ac0ab3951e2398039168f3cdf5bc02443113d
Jindong Yang <yangjindong@gmail.com>
2012-10-18 09:19:34 +0800

    add sanitize method to product title

---
+++ app/overrides/store_product_title.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/products/show",
+                     :name => "product_sanitize_title",
+                     :replace => "code[erb-loud]:contains('accurate_title')",
+                     :text => "<%= sanitize @product.name, :tags => %w(sup) %>",
+                     :disabled => false)
