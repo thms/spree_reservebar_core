6f4da8066a38dc9c07029e4e635b135dfd862124
Jindong Yang <yangjindong@gmail.com>
2012-10-20 21:07:10 +0800

    update and keep the captcha tag

--- app/overrides/store_email_to_friend.rb
+++ app/overrides/store_email_to_friend.rb
@@ -3,9 +3,3 @@ Deface::Override.new(:virtual_path => "spree/email_sender/send_mail",
                      :replace => "code[erb-loud]:contains('tell_about')",
                      :text => "<%= sanitize(t('email_to_friend.tell_about', :product => @object.name), :tags => %w(sup)) %>",
                      :disabled => false)
-                     
-Deface::Override.new(:virtual_path => "spree/email_sender/send_mail",
-                     :name => "product_email_sender_remove_captcha",
-                     :replace => "code[erb-loud]:contains('recaptcha_tags')",
-                     :text => "",
-                     :disabled => false)
