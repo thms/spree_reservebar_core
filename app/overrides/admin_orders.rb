66f742d6ec6a2d66c5e36bef3a608bc6857374d5
Jindong Yang <yangjindong@gmail.com>
2012-12-06 16:17:53 +0800

    add address info to order detail for admin

--- app/overrides/admin_orders.rb
+++ app/overrides/admin_orders.rb
@@ -3,3 +3,9 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :replace => "code[erb-loud]:contains('order_state')",
                      :partial => "spree/admin/shared/order_extended_state",
                      :disabled => false)
+                     
+Deface::Override.new(:virtual_path => "spree/admin/orders/show",
+                     :name => "admin_orders_address_is_business",
+                     :insert_after => "code[erb-loud]:contains('@order.ship_address')",
+                     :text => "<%= @order.ship_address.is_business ? 'This is a business address.' : 'This is a residential address.' %>",
+                     :disabled => false)
