300ec9b659e95f665b9bf4fef32eb110a10cb8e3
Jindong Yang <yangjindong@gmail.com>
2012-12-17 21:39:34 +0800

    add check_box :is_business to admin/shared/_address_form

--- app/overrides/admin_orders_address_type_selector.rb
+++ app/overrides/admin_orders_address_type_selector.rb
@@ -3,3 +3,9 @@ Deface::Override.new(:virtual_path => "spree/admin/orders/show",
                      :insert_after => "code[erb-loud]:contains('@order.ship_address')",
                      :text => "<%= @order.ship_address.is_business ? 'This is a business address.' : 'This is a residential address.' %>",
                      :disabled => false)
+                     
+Deface::Override.new(:virtual_path => "spree/admin/shared/_address_form",
+                     :name => "admin_orders_address_type",
+                     :insert_after => "code[erb-loud]:contains('f.text_field :phone')",
+                     :text => " <%= f.check_box :is_business %> This is a business address.",
+                     :disabled => false)
