6b014ce0d94924a3094c7b22efc1b49c10fc6d29
Thomas Boltze <thomas.boltze@gmail.com>
2013-01-03 14:22:08 +0800

    Added action to repsonse for credit card actions, so we can get the out from the logger

--- app/models/spree/creditcard_decorator.rb
+++ app/models/spree/creditcard_decorator.rb
@@ -20,6 +20,8 @@ Spree::Creditcard.class_eval do
       response = payment_gateway.credit((amount * 100).round, payment.response_code, options)
     end
 
+    ## Thomas: add action here, until we update to a later version of active merchant
+    response[:action] = "CREDIT" unless response[:action]
     record_log payment, response
 
     if response.success?
@@ -35,6 +37,103 @@ Spree::Creditcard.class_eval do
   rescue ActiveMerchant::ConnectionError => e
     gateway_error e
   end
+  
+  
+  ### THOMAS: The following three methods are duplicated here, so we can have the reponse set and logged, will become obsolete
+  ## once we update to Active Merchant 1.28 or higher
+  def authorize(amount, payment)
+    # ActiveMerchant is configured to use cents so we need to multiply order total by 100
+    payment_gateway = payment.payment_method
+    check_environment(payment_gateway)
+
+    response = payment_gateway.authorize((amount * 100).round, self, gateway_options(payment))
+    ## Thomas: add action here, until we update to a later version of active merchant
+    response[:action] = "AUTHORIZE" unless response[:action]
+    record_log payment, response
+
+    if response.success?
+      payment.response_code = response.authorization
+      payment.avs_response = response.avs_result['code']
+      payment.pend
+    else
+      payment.failure
+      gateway_error(response)
+    end
+  rescue ActiveMerchant::ConnectionError => e
+    gateway_error e
+  end
+
+  def purchase(amount, payment)
+    #combined Authorize and Capture that gets processed by the ActiveMerchant gateway as one single transaction.
+    payment_gateway = payment.payment_method
+    check_environment(payment_gateway)
+
+    response = payment_gateway.purchase((amount * 100).round, self, gateway_options(payment))
+    ## Thomas: add action here, until we update to a later version of active merchant
+    response[:action] = "PURCHASE" unless response[:action]
+    record_log payment, response
+
+    if response.success?
+      payment.response_code = response.authorization
+      payment.avs_response = response.avs_result['code']
+      payment.complete
+    else
+      payment.failure
+      gateway_error(response) unless response.success?
+    end
+  rescue ActiveMerchant::ConnectionError => e
+    gateway_error e
+  end
+
+  def capture(payment)
+    return unless payment.pending?
+    payment_gateway = payment.payment_method
+    check_environment(payment_gateway)
+
+    if payment_gateway.payment_profiles_supported?
+      # Gateways supporting payment profiles will need access to creditcard object because this stores the payment profile information
+      # so supply the authorization itself as well as the creditcard, rather than just the authorization code
+      response = payment_gateway.capture(payment, self, minimal_gateway_options(payment, false))
+    else
+      # Standard ActiveMerchant capture usage
+      response = payment_gateway.capture((payment.amount * 100).round, payment.response_code, minimal_gateway_options(payment, false))
+    end
+
+    ## Thomas: add action here, until we update to a later version of active merchant
+    response[:action] = "CAPTURE" unless response[:action]
+    record_log payment, response
+
+    if response.success?
+      payment.response_code = response.authorization
+      payment.complete
+    else
+      payment.failure
+      gateway_error(response)
+    end
+  rescue ActiveMerchant::ConnectionError => e
+    gateway_error e
+  end
+
+  def void(payment)
+    payment_gateway = payment.payment_method
+    check_environment(payment_gateway)
+
+    response = payment_gateway.void(payment.response_code, minimal_gateway_options(payment, false))
+    ## Thomas: add action here, until we update to a later version of active merchant
+    response[:action] = "VOID" unless response[:action]
+    record_log payment, response
+
+    if response.success?
+      payment.response_code = response.authorization
+      payment.void
+    else
+      gateway_error(response)
+    end
+  rescue ActiveMerchant::ConnectionError => e
+    gateway_error e
+  end
+  
+  #### END MONKEY PATCH
   
   
 end
