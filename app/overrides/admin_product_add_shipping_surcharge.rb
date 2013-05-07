f4555e535f530243df0f31c775cd17f9c65fefea
Thomas Boltze <thomas.boltze@gmail.com>
2013-05-07 18:16:47 +0800

    Force shipping surcharge after brand selector on product admin

--- app/overrides/admin_product_add_shipping_surcharge.rb
+++ app/overrides/admin_product_add_shipping_surcharge.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
 	                   :name => "admin_products_shipping_surcharge",
 	                   :insert_bottom => "[data-hook='admin_product_form_right']",
+	                   :sequence => {:after => "admin_products_brand_selector"}, 
 	                   :partial => "spree/admin/products/shipping_surcharge",
 	                   :disabled => false)
