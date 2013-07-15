9a5fcd9bfccc0550140c8e0dd4b9e3b8b2db7e74
Thomas Boltze <thomas.boltze@gmail.com>
2013-07-15 13:55:40 +0800

    Added Henessy DoubleClick tags for Gemeos

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -1,6 +1,7 @@
 Spree::PaymentMethod.class_eval do
 
-  #attr_accessible :name, :description, :environment, :display_on, :active, :type
+  ##attr_accessible :name, :description, :environment, :display_on, :active, :type, :deleted_at, :preferred_server
+  ## TODO: this seems to not be sufficient, potentially need the same or similar on the gateway class
   
 	def name_with_type
 		"#{type}: #{name}"
